67ec8a2d4a159fddf02519c894b48ee5e06fd3ee
Visal In <invisal@gmail.com>
2024-11-26 19:32:58 +0700

    add after sign notarization

---
+++ scripts/notarize.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const { notarize } = require("@electron/notarize");
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;
+  console.log("electronPlatformName = ", electronPlatformName);
+  console.log("appOutDir = ", appOutDir);
+  if (electronPlatformName !== "darwin") {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+
+  return await notarize({
+    tool: "notarytool",
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
+    teamId: process.env.APPLE_TEAM_ID,
+  });
+};
